45f6a2df3e9fa318c388725f27f737bcd7a21df9
Alemude <alemude21@gmail.com>
2023-07-14 08:51:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Alex</h1>
+      <input type="text" placeholder="enter your name"/>
+    </div>
+  );
+}
+
+export default App;
